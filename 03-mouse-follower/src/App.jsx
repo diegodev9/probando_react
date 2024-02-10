dd4826eb823cefaa3559ecb14b0fdb8158609b36
diegodev9 <diegodev9@gmail.com>
2024-02-10 18:32:56 -0300

    terminado proyecto 03

---
+++ 03-mouse-follower/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/bolita.css'
+import { FollowMouse } from './components/FollowMouse.jsx';
+
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
